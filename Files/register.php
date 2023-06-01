279834fa07a27e5c49f2042616125b4c5342eefe
devsfmin <devsfmin@icloud.com>
2023-06-01 16:39:21 +0900

    regist email try 1

--- Files/register.php
+++ Files/register.php
@@ -28,12 +28,20 @@ if (mysqli_connect_errno())//접근 실패 시
     exit(); }
 
 
-$sql_check = "SELECT * FROM new_table WHERE user_name='$user_name'";
-$request_name = mysqli_query($mysqli, $sql_check);
+$sql_check_N = "SELECT * FROM new_table WHERE user_name='$user_name'";
+$request_name = mysqli_query($mysqli, $sql_check_N);
+$sql_check_E = "SELECT * FROM new_table WHERE email='$email'";
+$request_email = mysqli_query($mysqli, $sql_check_E)
 
 //활동할 닉네임 중복 체크
 if (mysqli_num_rows($request_name) === 1){
     echo "이미 해당 닉네임이 존재합니다.";
+    echo "<a href=register.html> 돌아가기 </a>";
+    exit();
+}
+
+if (mysqli_num_rows($request_email) === 1){
+    echo "가입된 이메일이 존재합니다.";
     echo "<a href=register.html> 돌아가기 </a>";
     exit();
 }
