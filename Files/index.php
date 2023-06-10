4742773e50042588211e542c27fa5f2afde778ca
devsfmin <devsfmin@icloud.com>
2023-06-10 21:09:59 +0900

    session contents add 1-5

--- Files/index.php
+++ Files/index.php
@@ -10,13 +10,14 @@
         <title>Social Paper Main Page</title>
 
         <?php
-        if(!isset($_SESSION['email'])) {
+        if(!isset($_SESSION['user_name']) ||!isset($_SESSION['email'])) {
             //!isset($_SESSION['email']) || //
             echo "<p>로그인 가능</p>";} else {
                 $email = $_SESSION['email'];
-                // $user_name = $_SESSION['user_name'];
+                $user_name = $_SESSION['user_name'];
                 echo "<p><strong>$email</strong>님, 안녕하세요? 로그인 된 상태에요.</p>";
-                // echo "<p><strong>$user_name</strong>님, 안녕하세요? 로그인 된 상태에요.</p>";
+                echo "<p><strong>$user_name</strong>님, 안녕하세요? 로그인 된 상태에요.</p>";
+                echo "<a href=\"logout.php\">[로그아웃]</a></p>";
             }
             ?>
         <!-- 폰트 관련 -->
