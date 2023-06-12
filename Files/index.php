f36398311535bc92443f9a7831498426ebcf1669
devsfmin <devsfmin@icloud.com>
2023-06-12 20:21:51 +0900

    status checking test 3-3

--- Files/index.php
+++ Files/index.php
@@ -112,9 +112,9 @@
             $email = $_SESSION['email']; /* 가 아닌 세션 o 의 경우 */ ?>
                         
             <!-- Personal Section -->
-            <section class="py-2">
-                <div class="min-text">
-                    <h2> <?php echo $email; ?> 님. 안녕하세요! </h2>
+            <section class="py-5">
+                <div class="container px-4 px-lg-5 mt-5">
+                    <h2 class="masthead-heading text-uppercase mb-0"> <?php echo $email; ?> 님. 안녕하세요! </h2>
                 </div>
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
