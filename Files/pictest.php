910a977e7e79f4bc7ed25bc36ee9417bcb016d32
devsfmin <devsfmin@icloud.com>
2023-06-15 05:33:31 +0900

    BMP 4-1-12 echo mid

--- Files/pictest.php
+++ Files/pictest.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
-<?php 
-echo "asd";
-?>
 <html lang="en">
     <head>head</head>
     <body>
         
         body
-        <img src="https://usagi-post.com/wp-content/uploads/2020/05/no-image-found-360x250-1.png"/>
+        <img src="<?php echo "https://usagi-post.com/wp-content/uploads/2020/05/no-image-found-360x250-1.png"?>"/>
     </body>
