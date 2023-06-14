707751dd923205b4407573d7116434a53313a0c6
devsfmin <devsfmin@icloud.com>
2023-06-15 05:31:58 +0900

    BMP 4-1-10 img

--- Files/pictest.php
+++ Files/pictest.php
@@ -4,4 +4,8 @@
 ?>
 <html lang="en">
     <head>head</head>
-    <body>body</body>
+    <body>
+        
+        body
+        <img src="https://usagi-post.com/wp-content/uploads/2020/05/no-image-found-360x250-1.png"/>
+    </body>
